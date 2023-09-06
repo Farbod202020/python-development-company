27f706ac161fba00ac654c2b8980edd2f21a78da
Farbod202020 <143803764+Farbod202020@users.noreply.github.com>
2023-09-06 23:51:07 +0300

    Company theme
    Company template

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function HireUs() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function Products() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function GettoknowUs() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function LetUsHelpYou() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
